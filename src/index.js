a7cb2de84b3c5a3bb4af5de4cbe7e11c3a3419a1
Emily Huang <eh1691@nyu.edu>
2021-03-19 17:02:32 -0400

    creataed the root component aka Provider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>,
   rootElement
 );
